c3860a3eebcdc12dd6d84ecac0387448cf88abb2
Valentin <vtin88@yahoo.com>
2018-10-19 08:23:39 +0300

    Initial Commit

---
+++ TableViewGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewGallery
+//
+//  Created by mrbesford on 10/19/18.
+//  Copyright © 2018 mrbesford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
